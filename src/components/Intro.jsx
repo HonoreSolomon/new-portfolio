89f3ceafb1aad81a71b0cae9a592315b036856f0
Honore Solomon <HonoreSolomon@IT-Air-006.local>
2023-02-03 17:02:42 -0800

    init

---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <div
+      className='flex items-center
+  justify-center flex-col text-center pt-20
+  pb-6'
+    >
+      <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>
+        Honore
+      </h1>
+    </div>
+  );
+}
+
+export default Intro;
